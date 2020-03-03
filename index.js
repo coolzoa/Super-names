3094c2bc2182ff811587c79a2a5d884c29e5a58d
Jose Murillo <jp1995azul@hotmail.com>
2020-03-03 22:22:19 +0100

    Add partial views and templates with EJS, add Express

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
+//jshint esversion6;
+
 const superheroes = require("superheroes");
 const supervillains = require("supervillains");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-var random = superheroes.random();
-console.log(random);
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs')
+app.use(express.static("public"));
+
+//target the home route
+app.get("/", function(req, res) {
+  //no hero
+  res.render("index", {superName: "", superType: "hero"});
+});
+
+app.post("/", function(req, res) {
+  let superType = req.body.nameType;
+  let superName;
+  if (superType == "hero") {
+    superName = supervillains.random();
+  } else {
+    superName = superheroes.random();
+  }
+  res.render("index", {superName: superName, superType: superType});
+
+
+});
+
+
+
+
+
+
+
+app.get('*',function (req, res) {
+        res.redirect('/');
+    });
+
+app.listen(3000, function() {
+  console.log("Listening on port 3000");
+});
